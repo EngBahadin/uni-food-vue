ff2a9d45e728e46ef928dcd3d0b4b4502c5fca1f
Bahadin <bahaking341@gmail.com>
2025-10-20 03:14:21 +0300

    feat: adding routes for the pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import MenuView from '../views/MenuView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView,
+    },
+    {
+      path: '/menu',
+      name: 'Menu',
+      component: MenuView,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: AboutView,
+    },
+  ],
 })
 
 export default router
