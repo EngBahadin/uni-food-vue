a34802fb8dc2e9cf65a4d377fed80846a969096f
Bahadin <bahaking341@gmail.com>
2025-10-20 03:14:00 +0300

    feat: add Food interface and sample data, along with AboutView and HomeView components for enhanced site structure

---
+++ src/utils/foods.ts
@@ -0,0 +1,23 @@
+export interface Food {
+  id: number
+  name: string
+  image: string
+}
+
+export const foods: Food[] = [
+  {
+    id: 1,
+    name: 'Pizza',
+    image: '/pizza.png',
+  },
+  {
+    id: 3,
+    name: 'Cheese Burger',
+    image: '/cheese-burger.png',
+  },
+  {
+    id: 4,
+    name: 'Biryani',
+    image: '/bryani.png',
+  },
+]
---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="p-10 flex flex-col bg-white min-h-screen gap-y-14">
+    <div>
+      <h1 class="text-sub-heading-1-semiBold text-primary border-l-8 border-primary p-2 mb-4">
+        About
+      </h1>
+      <p class="text-body-3-regular text-black md:max-w-[70%]">
+        Unifood in an online ordering food website that serve students in college and allows them to
+        order food in class and eat it in their college cafeteria without waiting to prepare the
+        food
+      </p>
+    </div>
+    <img
+      src="/bro.png"
+      alt="Bro is ordering food"
+      class="object-contain md:w-[412px] md:h-96 w-96 h-80 self-end"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+// About page logic can be added here
+</script>
