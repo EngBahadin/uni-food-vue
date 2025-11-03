bec83f135b6d9952000cd2dc1754e28e2223da1d
Bahadin <bahaking341@gmail.com>
2025-11-03 04:05:04 +0300

    feat: implement dynamic category loading and error handling in MenuSection and CategorySection components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <HeroSection />
+    <MenuSection />
   </div>
 </template>
 
 <script setup lang="ts">
 import HeroSection from '../components/HeroSection.vue'
+import MenuSection from '../components/MenuSection.vue'
 </script>
