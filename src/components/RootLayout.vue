375950912a06bc2b1d061665e51ebfdf4cfc60d0
Ali Abdulkhaliq <alimajeed8110@gmail.com>
2025-11-22 19:04:39 +0300

    fix: update Navbar visibility logic and remove dark mode class from RootLayout

--- src/components/RootLayout.vue
+++ src/components/RootLayout.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="min-h-screen bg-white dark:bg-black transition-colors duration-300">
-    <Navbar />
+  <div class="min-h-screen bg-white transition-colors duration-300">
+    <Navbar v-if="!isAuthPage" />
     <main>
       <router-view />
     </main>
@@ -8,5 +8,13 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import Navbar from './NavbarSection.vue'
+
+const route = useRoute()
+
+const isAuthPage = computed(() => {
+  return route.path.startsWith('/auth')
+})
 </script>
